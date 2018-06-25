4cfa6ec5bf005297880c09dbb7e18b8b26be1fdb
Keir Staple <keirstaple@comcast.net>
2018-06-25 02:01:40 -0400

    fix ie11 store issue

--- src/store.js
+++ src/store.js
@@ -9,14 +9,17 @@ import rootSaga from './state/sagas';
 const sagaMiddleware = createSagaMiddleware();
 const browserHistory = createBrowserHistory();
 
+const composeSetup = process.env.NODE_ENV !== 'production' && typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
+
 const store = createStore(
   rootReducer,
-  compose(
+  composeSetup(
     applyMiddleware(
       routerMiddleware(browserHistory),
       sagaMiddleware,
     ),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 sagaMiddleware.run(rootSaga);
