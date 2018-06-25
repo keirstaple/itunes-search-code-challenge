d8ba806a93bd216832aebefd79569e5954a0f4e3
Keir Staple <keirstaple@comcast.net>
2018-06-25 03:15:36 -0400

    remove router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import store from './store';
 import './styles/index.css';
@@ -9,13 +8,9 @@ import Home from './views/Home';
 import registerServiceWorker from './utils/registerServiceWorker';
 
 const App = () => (
-  <Router>
-    <Provider store={store}>
-      <div>
-        <Route exact path='/' component={Home} />
-      </div>
-    </Provider>
-  </Router>
+  <Provider store={store}>
+    <Home />
+  </Provider>
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
